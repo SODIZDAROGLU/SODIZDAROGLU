7e8e4f41b35015ff0dd81b367bd82663f70adbc3
sodizdaroglu <americanlaz@gmail.com>
2023-02-23 16:57:12 -0500

    deleteHeroku

--- server.js
+++ server.js
@@ -10,13 +10,16 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", contactRoute);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("portfolio-site/build"));
+app.use(express.static("portfolio-site/build"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "portfolio-site", "build", "index.html"))
   );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("portfolio-site/build"));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "portfolio-site", "build", "index.html"))
+//   );
+// }
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`server listing to port 3000 only`));
