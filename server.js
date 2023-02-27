1a2964f54661501eb0dddf100529e4fad35471ef
sodizdaroglu <americanlaz@gmail.com>
2023-02-27 12:14:16 -0500

    test5

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", contactRoute);
+//app.use("/", contactRoute);
 
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
   app.use(express.static("portfolio-site/build"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "portfolio-site", "build", "index.html"))
   );
-}
+//}
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`server listing to port 3000 only`));
